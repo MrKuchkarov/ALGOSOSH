66559aa089ec2ab1c52ee1ab763e26bd62ea55ab
Mukhammad <yekut1515@yandex.ru>
2024-01-18 16:53:55 +0500

    fixed: Final code editing

--- src/utils/delay.ts
+++
@@ -1,2 +0,0 @@
-// A function that returns a Promise to introduce a delay in asynchronous operations
-export const delay = (ms: number): Promise<void> => new Promise(resolve => setTimeout(resolve, ms));
