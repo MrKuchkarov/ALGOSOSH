ae21c6c0067a03b973eb0743f68a0298cdf45593
Mukhammad <yekut1515@yandex.ru>
2024-02-13 11:54:54 +0300

    create: Wrote tests for buttons in the project

---
+++ src/components/ui/button/button.test.tsx
@@ -0,0 +1,48 @@
+import {fireEvent, render, screen, act} from "@testing-library/react";
+import { Button } from "./button";
+import { Direction } from "../../../types/direction";
+import React from "react";
+describe("Testing button component", () => {
+    it("button with text", () => {
+        const { container } = render(<Button text="test text" />);
+        expect(container.firstChild).toMatchSnapshot();
+    });
+
+    it("button without text", () => {
+        const { container } = render(<Button />);
+        expect(container.firstChild).toMatchSnapshot();
+    });
+
+    it("button disabled", () => {
+        const { container } = render(<Button disabled />);
+        expect(container.firstChild).toMatchSnapshot();
+    });
+
+    it("button has loading", () => {
+        const { container } = render(<Button isLoader={true} />);
+        expect(container.firstChild).toMatchSnapshot();
+    });
+
+    it("Button has Ascending", () => {
+        const { container } = render(<Button sorting={Direction.Ascending}/>);
+        expect(container.firstChild).toMatchSnapshot();
+    });
+
+    it("Button has Descending", () => {
+        const { container } = render(<Button sorting={Direction.Descending}/>);
+        expect(container.firstChild).toMatchSnapshot();
+    });
+
+    it("Button has sizes", () => {
+        const { container } = render(<Button sizes="small" />);
+        expect(container.firstChild).toMatchSnapshot();
+    });
+
+    it("Callback should work correctly after click", () => {
+        const cb = jest.fn();
+        render(<Button onClick={cb} />);
+        const button = screen.getByRole("button");
+        fireEvent.click(button);
+        expect(cb).toHaveBeenCalled();
+    });
+});
