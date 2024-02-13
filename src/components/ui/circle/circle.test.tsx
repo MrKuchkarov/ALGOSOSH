c43ffa12f664de2eda797b54258c815f23b60600
Mukhammad <yekut1515@yandex.ru>
2024-02-13 19:03:00 +0300

    fixed: Moved the logic into a separate function Circle component

--- src/components/ui/circle/circle.test.tsx
+++ src/components/ui/circle/circle.test.tsx
@@ -2,49 +2,56 @@ import {render} from "@testing-library/react";
 import {Circle} from "./circle";
 import {ElementStates} from "../../../types/element-states";
 
+type CircleProps = {
+    letter?: string;
+    head?: string | JSX.Element;
+    tail?: string | JSX.Element;
+    index?: number;
+    isSmall?: boolean;
+    state?: ElementStates;
+}
+
 describe("Testing circle component", () => {
-    it("Should be without a letter", () => {
-        const {container} = render(<Circle/>);
+    const testSnapshot = (props: CircleProps) => {
+        const { container } = render(<Circle {...props} />);
         expect(container.firstChild).toMatchSnapshot();
+    };
+    it("Should be without a letter", () => {
+        testSnapshot({});
     });
     it("Should be with a letter", () => {
-        const {container} = render(<Circle letter={"a"}/>);
-        expect(container.firstChild).toMatchSnapshot();
+        testSnapshot({ letter: "a" })
     });
     it("Should be with head", () => {
-        const {container} = render(<Circle head={"7"}/>);
-        expect(container.firstChild).toMatchSnapshot();
+        testSnapshot({ head: "7" })
     });
     it("Should be with a react element in head", () => {
-        const {container} = render(<Circle head={<Circle/>}/>);
-        expect(container.firstChild).toMatchSnapshot();
+        testSnapshot({ head: <Circle /> })
     });
     it("Should be with tail", () => {
-        const {container} = render(<Circle tail={"7"}/>);
-        expect(container.firstChild).toMatchSnapshot();
+        testSnapshot({ tail: "7" })
     });
     it("Should be with a react element in tail", () => {
-        const {container} = render(<Circle tail={<Circle/>}/>);
-        expect(container.firstChild).toMatchSnapshot();
+        testSnapshot({ tail: <Circle /> });
     });
+
     it("Should be with index", () => {
-        const { container } = render(<Circle index={0}/>);
-        expect(container.firstChild).toMatchSnapshot();
+        testSnapshot({ index: 0 });
     });
+
     it("Props isSmall should be true", () => {
-        const { container } = render(<Circle isSmall={true}/>);
-        expect(container.firstChild).toMatchSnapshot();
+        testSnapshot({ isSmall: true });
     });
+
     it("Should be default state", () => {
-        const { container } = render(<Circle state={ElementStates.Default}/>);
-        expect(container.firstChild).toMatchSnapshot();
+        testSnapshot({ state: ElementStates.Default });
     });
+
     it("Should be changing state", () => {
-        const { container } = render(<Circle state={ElementStates.Changing} />);
-        expect(container.firstChild).toMatchSnapshot();
+        testSnapshot({ state: ElementStates.Changing });
     });
+
     it("Should be modified state", () => {
-        const { container } = render(<Circle state={ElementStates.Modified}/>);
-        expect(container.firstChild).toMatchSnapshot();
+        testSnapshot({ state: ElementStates.Modified });
     });
 });
