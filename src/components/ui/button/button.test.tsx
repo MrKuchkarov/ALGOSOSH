ecca67ebd78074fe4c3bdbc055a40ea668f59772
Mukhammad <yekut1515@yandex.ru>
2024-02-13 13:05:53 +0300

    fixed: rewrote test to it

--- src/components/ui/button/button.test.tsx
+++ src/components/ui/button/button.test.tsx
@@ -3,42 +3,42 @@ import { Button } from "./button";
 import { Direction } from "../../../types/direction";
 import React from "react";
 describe("Testing button component", () => {
-    test("button with text", () => {
+    it("Button with text", () => {
         const { container } = render(<Button text="test text" />);
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test("button without text", () => {
+    it("Button without text", () => {
         const { container } = render(<Button />);
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test("button disabled", () => {
+    it("Button disabled", () => {
         const { container } = render(<Button disabled />);
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test("button has loading", () => {
+    it("Button has loading", () => {
         const { container } = render(<Button isLoader={true} />);
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test("Button has Ascending", () => {
+    it("Button has Ascending", () => {
         const { container } = render(<Button sorting={Direction.Ascending}/>);
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test("Button has Descending", () => {
+    it("Button has Descending", () => {
         const { container } = render(<Button sorting={Direction.Descending}/>);
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test("Button has sizes", () => {
+    it("Button has sizes", () => {
         const { container } = render(<Button sizes="small" />);
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    test("Callback should work correctly after click", () => {
+    it("Callback should work correctly after click", () => {
         const cb = jest.fn();
         render(<Button onClick={cb} />);
         const button = screen.getByRole("button");
