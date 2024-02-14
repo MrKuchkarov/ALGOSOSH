cdc232b1137fb42491c21e3cd0aa3fb905bb823e
Mukhammad <yekut1515@yandex.ru>
2024-02-14 13:44:31 +0300

    create: wrote a test for an empty array, with one element and elements for sort bubble ascending and descending

--- src/pages/sorting-page/sorting-page.test.tsx
+++ src/pages/sorting-page/sorting-page.test.tsx
@@ -9,9 +9,21 @@ import {
 import {delay} from "../../utils/delay";
 
 jest.setTimeout(30000);
-const setArray= jest.fn();
-const setActive = jest.fn();
-const setIsReversed = jest.fn();
+let setArray: jest.Mock;
+let setActive: jest.Mock;
+let setIsReversed: jest.Mock;
+
+beforeEach(() => {
+    setArray = jest.fn();
+    setActive = jest.fn();
+    setIsReversed = jest.fn();
+});
+
+afterEach(() => {
+    setArray.mockReset();
+    setActive.mockReset();
+    setIsReversed.mockReset();
+});
 
 describe("Selection sort ascending", () => {
     it("Should be correct with empty array", async () => {
@@ -42,3 +54,33 @@ describe("Selection sort descending", () => {
         expect(setArray).toHaveBeenLastCalledWith(resultArrayWithItemsDesc)
     });
 });
+
+describe("Bubble sort ascending", () => {
+    it("Should be correct with empty array", async () => {
+        await bubbleSortAscending([], setArray, setActive, setIsReversed, delay);
+        expect(setArray).toHaveBeenCalledTimes(0);
+    });
+    it("Should be correct with only one item", async () => {
+        await bubbleSortAscending(arrayWithOneItem, setArray, setActive, setIsReversed);
+        expect(setArray).toHaveBeenCalledTimes(0);
+    });
+    it("Should be correct with items", async () => {
+        await bubbleSortAscending(arrayWithItems, setArray, setActive, setIsReversed);
+        expect(setArray).toHaveBeenLastCalledWith(resultArrayWithItemsDesc);
+    });
+});
+
+describe("Bubble sort descending", () => {
+    it("Should be correct with empty array", async () => {
+        await bubbleSortDescending([], setArray, setActive, setIsReversed, delay);
+        expect(setArray).toHaveBeenCalledTimes(0);
+    });
+    it("Should be correct with only one item", async () => {
+        await bubbleSortDescending(arrayWithOneItem, setArray, setActive, setIsReversed);
+        expect(setArray).toHaveBeenCalledTimes(0);
+    });
+    it("Should be correct with items", async () => {
+        await bubbleSortDescending(arrayWithItems, setArray, setActive, setIsReversed);
+        expect(setArray).toHaveBeenLastCalledWith(resultArrayWithItemsAsc);
+    });
+})
