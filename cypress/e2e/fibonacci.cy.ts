6b009e1ee4d031036965ca2bbeb4a62e7f087d0e
Mukhammad <yekut1515@yandex.ru>
2024-02-15 14:38:41 +0300

    create: Wrote an e2e test in Cypress for the fibonacci component

---
+++ cypress/e2e/fibonacci.cy.ts
@@ -0,0 +1,23 @@
+import {TEST_URL} from "../../src/constants/testing";
+describe("fibonacci", () => {
+    beforeEach(() => {
+        cy.visit(`${TEST_URL}/fibonacci`);
+    })
+
+    it("Button should be disabled is string is empty", () => {
+        cy.get("input").should("be.empty");
+        cy.get('button[type="submit"]').should("be.disabled");
+    });
+    it("Numbers should be generated correctly", () => {
+        cy.get("input").type("3");
+        cy.contains("Расчитать").click();
+
+        const expectedNumbers: number[] = [1, 1, 2, 3];
+        cy.get("[class^=circle_circle]")
+            .should("have.length", expectedNumbers.length)
+            .each((el, index) => {
+                const expectedNumber = expectedNumbers[index];
+                expect(el).to.contain(expectedNumber.toString());
+            })
+    });
+})
