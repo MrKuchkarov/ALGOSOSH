99daecffe19b3eb54521d2285e7d5ac3288fa00a
Mukhammad <yekut1515@yandex.ru>
2024-02-18 12:09:58 +0300

    fixed: adding e2e test at the String component, should reverse correctly for even length strings

--- cypress/e2e/string.cy.ts
+++ cypress/e2e/string.cy.ts
@@ -1,5 +1,5 @@
 import {TEST_URL} from "../../src/constants/testing";
-import {DELAY_IN_MS} from "../../src/constants/delays";
+import {DELAY_IN_MS, DELAY_IN_MS_LONG} from "../../src/constants/delays";
 describe("String", () => {
     beforeEach(() => {
         cy.visit(`${TEST_URL}/iterative`);
@@ -39,5 +39,37 @@ describe("String", () => {
                 if (index === 1) expect(el).to.contain("u");
                 if (index === 2) expect(el).to.contain("r");
             });
+    });
+
+    it("Should reverse correctly for even length strings", () => {
+        cy.get("input").type("love");
+        cy.contains("Развернуть").click();
+
+        cy.get("[class^=circle_circle]").as("circle");
+
+        cy.get("@circle").each((el, index) => {
+            if (index === 0 || index === 3) {
+                cy.wrap(el).should(
+                    "have.css",
+                    "border",
+                    "4px solid rgb(210, 82, 225)"
+                );
+            }
+            if (index === 0) expect(el).to.contain("l");
+            if (index === 3) expect(el).to.contain("e");
+        });
+        cy.wait(DELAY_IN_MS_LONG);
+
+        cy.get("@circle").each((el, index) => {
+            cy.wrap(el).should(
+                "have.css",
+                "border",
+                "4px solid rgb(127, 224, 81)"
+            );
+            if (index === 0) expect(el).to.contain("e");
+            if (index === 1) expect(el).to.contain("v");
+            if (index === 2) expect(el).to.contain("o");
+            if (index === 3) expect(el).to.contain("l");
+        });
     });
 })
