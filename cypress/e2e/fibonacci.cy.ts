053981b8b8ef904b3606357e69347fdcb5f90473
Mukhammad <yekut1515@yandex.ru>
2024-02-20 10:36:37 +0300

    fixed: On the Fibonacci page I added 2 more e2e tests, with different values

--- cypress/e2e/fibonacci.cy.ts
+++ cypress/e2e/fibonacci.cy.ts
@@ -1,4 +1,5 @@
 import {TEST_URL} from "../../src/constants/testing";
+import {DELAY_IN_MS, DELAY_IN_MS_LONG_LONG} from "../../src/constants/delays";
 describe("fibonacci", () => {
     beforeEach(() => {
         cy.visit(`${TEST_URL}/fibonacci`);
@@ -13,6 +14,30 @@ describe("fibonacci", () => {
         cy.contains("Расчитать").click();
 
         const expectedNumbers: any = [1, 1, 2, 3];
+        cy.get("[class^=circle_circle]")
+            .should("have.length", expectedNumbers.length)
+            .each((el, index) => {
+                const expectedNumber = expectedNumbers[index];
+                expect(el).to.contain(expectedNumber.toString());
+            })
+    });
+    it("Numbers should be generated correctly", () => {
+        cy.get("input").type("7");
+        cy.contains("Расчитать").click();
+        cy.wait(DELAY_IN_MS);
+        const expectedNumbers: any = [1, 1, 2, 3, 5, 8, 13, 21];
+        cy.get("[class^=circle_circle]")
+            .should("have.length", expectedNumbers.length)
+            .each((el, index) => {
+                const expectedNumber = expectedNumbers[index];
+                expect(el).to.contain(expectedNumber.toString());
+            })
+    });
+    it("Numbers should be generated correctly", () => {
+        cy.get("input").type("13");
+        cy.contains("Расчитать").click();
+        cy.wait(DELAY_IN_MS_LONG_LONG);
+        const expectedNumbers: any = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377];
         cy.get("[class^=circle_circle]")
             .should("have.length", expectedNumbers.length)
             .each((el, index) => {
